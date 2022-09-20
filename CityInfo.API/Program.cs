1a326e551a0d43b4f83f6174962d90e069bd8ca2
Christopher Dubbs <dubbschristopher@gmail.com>
2022-09-20 06:55:21 -0700

    updated

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.StaticFiles;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args) // Setup host
 
 // Add services to the container.
 
